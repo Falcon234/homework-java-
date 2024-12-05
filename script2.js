f606ab68934a2e21c1228806a20f4254203c04e0
Falcon234 <131895444+Falcon234@users.noreply.github.com>
2024-12-05 18:39:50 +0200

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,14 @@
+console.log(age);
+let userName = "Nikita";
+console.log(userName);
+let isStudent = "true";
+console.log(isStudent);
+let myString = "Trust me pilot...";
+console.log(myString);
+let myNumber = 1488;
+console.log(myNumber+10);
+let myNull = null()
+console.log(myNull);
+let usersName = prompt("Enter ur name please")
+alert(`Це ви ${usersName}`)
+console.log(typeof usersName);
